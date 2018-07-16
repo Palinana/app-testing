ffb79d2b5deef4afb59fe661a7a38cb6f4943523
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2018-07-16 16:37:40 -0400

    basic app setup is done

--- flashcardpro/src/components/App.js
+++ flashcardpro/src/components/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import StackList from './StackList';
 
-const App = () => {
-  return (
-    <div>
-      <h2>Flashcard Pro</h2>
-      <hr/>
-      <StackList />
-      <hr />
-      <Link to='stack_form'><h4>Create a New Stack</h4></Link>
-    </div>
-  )
+class App extends Component{
+    render() {
+        return (
+            <div>
+                <h2>FlashCard Pro</h2>
+                <hr/>
+                <StackList />
+            </div>
+        )
+    }
 }
 
 export default App;
