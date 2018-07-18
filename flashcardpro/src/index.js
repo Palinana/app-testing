385c5b1002744bd89618261a43c339053975795a
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2018-07-17 21:17:44 -0400

    added redux to the app

--- flashcardpro/src/index.js
+++ flashcardpro/src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import rootReducer from './reducers';
 import App from './components/App';
 import Stack from './components/Stack';
+import { setStack } from './actions';
+
+const store = createStore(rootReducer);
+store.subscribe(() => console.log('store', store.getState()));
+store.dispatch(setStack({id: 1, title: 'example', cards: []}));
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={App}/>
-            <Route path="/stack" component={Stack}/>
-        </Switch>
-    </BrowserRouter>, 
-    document.getElementById('root')
+    <Provider store={store}>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={App}/>
+                <Route path="/stack" component={Stack}/>
+            </Switch>
+        </BrowserRouter>
+    </Provider>, 
+    document.getElementById('root')   
 );
