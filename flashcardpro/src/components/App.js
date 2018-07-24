c401d21ea6e2ce070fa8a6517ecad586707098f4
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2018-07-24 17:53:04 -0400

    made stateless components

--- flashcardpro/src/components/App.js
+++ flashcardpro/src/components/App.js
@@ -2,18 +2,16 @@ import React, { Component } from 'react';
 import StackList from './StackList';
 import { Link } from 'react-router-dom';
 
-class App extends Component{
-    render() {
-        return (
-            <div>
-                <h2>FlashCard Pro</h2>
-                <hr/>
-                <StackList />
-                <hr/>
-                <Link to="stack_form"><h4>Create a New Stack</h4></Link>
-            </div>
-        )
-    }
+const App = () => {    
+    return (
+        <div>
+            <h2>FlashCard Pro</h2>
+            <hr/>
+            <StackList />
+            <hr/>
+            <Link to="stack_form"><h4>Create a New Stack</h4></Link>
+        </div>
+    )
 }
 
 export default App;
--- flashcardpro/src/components/Stack.js
+++ flashcardpro/src/components/Stack.js
@@ -3,26 +3,24 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Card from './Card';
 
-export class Stack extends Component{
-    render() {
-        const { title, cards } = this.props.stack;
-        return (
-            <div>
-                <Link className="link-home" to="/">
-                    <h4>Back</h4>
-                </Link>
-                <h3>{title}</h3>
-                <br />
-                {
-                    cards.map(card => {
-                       return (
-                           <Card key={card.id} card={card}/>
-                       ) 
-                    })
-                }
-            </div>
-        )
-    }
+export const Stack = (props) => {
+    const { title, cards } = props.stack;
+    return (
+        <div>
+            <Link className="link-home" to="/">
+                <h4>Back</h4>
+            </Link>
+            <h3>{title}</h3>
+            <br />
+            {
+                cards.map(card => {
+                    return (
+                        <Card key={card.id} card={card}/>
+                    ) 
+                })
+            }
+        </div>
+    )
 }
 
 function mapStateToProps(state){
